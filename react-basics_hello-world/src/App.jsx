e3af0de3f5110ee0ed19366e1774d157c2a96830
Lisa Hauschild <lisa_hauschild@hotmail.com>
2025-02-28 12:30:58 +0100

    first commit

---
+++ react-basics_hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return HelloWorld();
+}
+
+
+
+//To do so, create a component (i.e. function) called `HelloWorld`.
+
+
+function HelloWorld() {
+  return(
+   <h1>Hello World 大家好！</h1>);
+}
+
+
+
